568b139b83c0283cbd970a078d65f78a0ad5e2cd
Marc Dürst <marc@marcduerst.com>
2016-12-23 14:10:14 +0100

    Updatet to NetStandard 1.6 and added an entity

---
+++ src/Tauchbolde.Common/Entities/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Tauchbolde.Common.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Organisator { get; set; }
+        public string Location { get; set; }
+        public string MeetingPoint { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
