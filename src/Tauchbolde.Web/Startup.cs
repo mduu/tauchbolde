92338b66fc9ecbc2b72a11a82bd7aa8015db35c9
Marc Dürst <marc@marcduerst.com>
2017-02-04 13:34:06 +0100

    Added EF startup

--- src/Tauchbolde.Web/Startup.cs
+++ src/Tauchbolde.Web/Startup.cs
@@ -29,6 +29,7 @@ namespace Tauchbolde.Web
         {
             // Add framework services.
             services.AddMvc();
+            services.AddEntityFrameworkSqlServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
