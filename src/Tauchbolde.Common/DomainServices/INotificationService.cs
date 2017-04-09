5c9201f50ceae06edbfdd2337854a4784adebe99
Marc Dürst <marc@marcduerst.com>
2017-04-09 16:16:00 +0200

    Draft of NotificationService API
    #1

---
+++ src/Tauchbolde.Common/DomainServices/INotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Tauchbolde.Common.Model;
+using Tauchbolde.Common.Repositories;
+
+namespace Tauchbolde.Common.DomainServices
+{
+    public interface INotificationService
+    {
+        Task NotifyForNewEventAsync(INotificationRepository notificationRepository, Event newEvent);
+        Task NotifyForChangedEventAsync(INotificationRepository notificationRepository, Event changedEvent);
+        Task NotifyForChangedParticipation(INotificationRepository notificationRepository, Participant participant);
+        Task NotifyForEventComment(INotificationRepository notificationRepository, Comment comment);
+        Task NotifyForNewPost(INotificationRepository notificationRepository, Post newPost);
+    }
+}
---
+++ src/Tauchbolde.Common/DomainServices/NotificationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tauchbolde.Common.Model;
+using Tauchbolde.Common.Repositories;
+
+namespace Tauchbolde.Common.DomainServices
+{
+    public class NotificationService : INotificationService
+    {
+        public async Task NotifyForNewEventAsync(INotificationRepository notificationRepository, Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task NotifyForChangedEventAsync(INotificationRepository notificationRepository, Event changedEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task NotifyForChangedParticipation(INotificationRepository notificationRepository, Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task NotifyForEventComment(INotificationRepository notificationRepository, Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task NotifyForNewPost(INotificationRepository notificationRepository, Post newPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
