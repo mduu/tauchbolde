7d811220f057e31439e07382bd51a0ffee7cc3cf
Marc Dürst <marc@marcduerst.com>
2018-09-26 20:48:00 +0200

    F Add ApplicationDbContextFactory

---
+++ src/Tauchbolde.Web/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Tauchbolde.Common.Model;
+
+namespace Tauchbolde.Web
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlite("Data Source=app.db");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
