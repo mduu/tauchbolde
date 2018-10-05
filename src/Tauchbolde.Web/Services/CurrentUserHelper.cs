3ea83edde0e34e0c82ed929a2499b5ee744e8764
Marc Dürst <marc@marcduerst.com>
2018-10-05 16:43:16 +0200

    r Rename applicationUserRepository to diverRepository

--- src/Tauchbolde.Web/Services/CurrentUserHelper.cs
+++ src/Tauchbolde.Web/Services/CurrentUserHelper.cs
@@ -15,14 +15,14 @@ namespace Tauchbolde.Web.Services
         /// Get the user enttity instance for the currenctly logged in user.
         /// </summary>
         /// <param name="controller">The MVC controller used to access the identity.</param>
-        /// <param name="applicationUserRepository">The <see cref="IDiverRepository"/> used to access the database.</param>
+        /// <param name="diverRepository">The <see cref="IDiverRepository"/> used to access the database.</param>
         /// <returns>Return the <see cref="Diver"/> instance or <c>Null</c> if none was found.</returns>
-        public static async Task<Diver> GetCurrentUserAsync(this Controller controller, IDiverRepository applicationUserRepository)
+        public static async Task<Diver> GetCurrentUserAsync(this Controller controller, IDiverRepository diverRepository)
         {
             if (controller == null) throw new ArgumentNullException(nameof(controller));
-            if (applicationUserRepository == null) throw new ArgumentNullException(nameof(applicationUserRepository));
+            if (diverRepository == null) throw new ArgumentNullException(nameof(diverRepository));
 
-            return await applicationUserRepository.FindByUserNameAsync(controller.User.Identity.Name);
+            return await diverRepository.FindByUserNameAsync(controller.User.Identity.Name);
         }
     }
 }
