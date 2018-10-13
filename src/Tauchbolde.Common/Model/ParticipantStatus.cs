b2efdcb479b72952c3590ed8c88c6fe96c612321
Marc Dürst <marc@marcduerst.com>
2018-10-13 20:58:50 +0200

    B Fix german translations for participation status

--- src/Tauchbolde.Common/Model/ParticipantStatus.cs
+++ src/Tauchbolde.Common/Model/ParticipantStatus.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tauchbolde.Common.Model
 {
     public enum ParticipantStatus
     {
+        [Display(Name = "Unbekannt")]
         None,
+        [Display(Name = "Bin dabei")]
         Accepted,
+        [Display(Name = "Kann nicht")]
         Declined,
+        [Display(Name = "Noch unsicher")]
         Tentative,
     }
 }
