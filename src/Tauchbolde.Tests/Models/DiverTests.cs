0a4beba9a502c33261e2595e95d45c6adffe1150
Marc Dürst <marc@marcduerst.com>
2018-10-15 16:37:41 +0200

    t Add tests for Diver.GetTwitterUrl()

---
+++ src/Tauchbolde.Tests/Models/DiverTests.cs
@@ -0,0 +1,22 @@
+﻿using Tauchbolde.Common.Model;
+using Xunit;
+
+namespace Tauchbolde.Tests.Mocks
+{
+    public class DiverTests
+    {
+        [Theory]
+        [InlineData("myname", "myname")]
+        [InlineData("@myname", "myname")]
+        [InlineData("_myname", "_myname")]
+        [InlineData("@@myname", "@myname")]
+        [InlineData("", "")]
+        public void GetTwitterUrl(string twitterHandle, string expectedUrl)
+        {
+            var diver = new Diver { TwitterHandle = twitterHandle };
+            var twitterUrl = diver.GetTwitterUrl();
+
+            Assert.Equal(expectedUrl, twitterUrl);
+        }
+    }
+}
