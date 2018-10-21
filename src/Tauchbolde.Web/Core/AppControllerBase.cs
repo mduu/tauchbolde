7d42b407e5eb74fd958a668b4cac1870af2c0fec
Marc Dürst <marc@marcduerst.com>
2018-10-21 22:07:11 +0200

    F Add base controller that can render success and error messages

---
+++ src/Tauchbolde.Web/Core/AppControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace Tauchbolde.Web.Core
+{
+    public abstract class AppControllerBase: Controller
+    {
+        protected void ShowSuccessMessage(string message)
+        {
+            TempData["success_message"] = message;
+        }
+        
+        protected void ShowErrorMessage(string message)
+        {
+            TempData["error_message"] = message;
+        }
+    }
+}
