9694b7b4d1f6fcfaebed22f1fc010fac6a22756a
Marc Dürst <marc@marcduerst.com>
2018-11-06 07:11:43 +0100

    Add ApplicationInsights

--- src/Tauchbolde.Web/Program.cs
+++ src/Tauchbolde.Web/Program.cs
@@ -12,6 +12,7 @@ namespace Tauchbolde.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
