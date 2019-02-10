d6b96a76439959219491f07a507c755fa164b31e
Marc Dürst <marc@marcduerst.com>
2019-02-10 21:45:56 +0100

    !!! Refactor and extract Participant Component
    #42

---
+++ src/Tauchbolde.Web/ViewComponents/ParticipantComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Tauchbolde.Common.Model;
+using Tauchbolde.Web.Models.ViewComponentModels;
+
+namespace Tauchbolde.Web.ViewComponents
+{
+    public class ParticipantComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke(Participant participant)
+        {
+            return View(new ParticipantViewModel
+            {
+                Diver = participant.ParticipatingDiver,
+                Name = participant.ParticipatingDiver.Realname,
+                CountPeople = participant.CountPeople,
+                Notes = participant.Note,
+                Status = participant.Status,
+            });
+        }
+    }
+}
