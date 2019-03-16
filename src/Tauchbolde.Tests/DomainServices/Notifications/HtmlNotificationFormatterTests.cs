6f69f2b33133c21a383c285fc2ccfd349afa069d
Marc Dürst <marc@marcduerst.com>
2019-03-16 20:54:51 +0100

    r Refactor HtmlNotificationFormatterTests

--- src/Tauchbolde.Tests/DomainServices/Notifications/HtmlNotificationFormatterTests.cs
+++ src/Tauchbolde.Tests/DomainServices/Notifications/HtmlNotificationFormatterTests.cs
@@ -14,6 +14,24 @@ namespace Tauchbolde.Tests.DomainServices.Notifications
 {
     public class HtmlNotificationFormatterTests
     {
+        private readonly HtmlFormatter formatter;
+
+        public HtmlNotificationFormatterTests()
+        {
+            var urlGenerator1 = A.Fake<IUrlGenerator>();
+            A.CallTo(() => urlGenerator1.GenerateEventUrl(A<Guid>._))
+                .Returns("http://test/event/1");
+
+            var notificationListFormatter = new HtmlListFormatter(
+                urlGenerator1,
+                new NotificationTypeInfos());
+
+            formatter = new HtmlFormatter(
+                new HtmlHeaderFormatter(),
+                notificationListFormatter,
+                new HtmlFooterFormatter());
+        }
+
         [Theory]
         [InlineData(NotificationType.NewEvent)]
         [InlineData(NotificationType.CancelEvent)]
@@ -29,31 +47,8 @@ namespace Tauchbolde.Tests.DomainServices.Notifications
             using (ApprovalResults.ForScenario(notificationType.ToString()))
             {
                 // Arrange
-                var urlGenerator = A.Fake<IUrlGenerator>();
-                A.CallTo(() => urlGenerator.GenerateEventUrl(A<Guid>._)).Returns("http://test/event/1");
-                
-                var headerFormatter = new HtmlHeaderFormatter();
-                var notificationTypeInfo = new NotificationTypeInfos();
-                var notificationListFormatter = new HtmlListFormatter(urlGenerator, notificationTypeInfo);
-                var footerFormatter = new HtmlFooterFormatter();
-                var formatter = new HtmlFormatter(headerFormatter, notificationListFormatter, footerFormatter);
-                var receiver = new Diver
-                {
-                    Id = new Guid("4c3b714e-522f-4ef8-85f4-db74f0ccdd76"),
-                    Fullname = "John Doe",
-                    Firstname = "John",
-                    Lastname = "Doe",
-                };
-                
-                var notifications = new List<Notification>()
-                {
-                    new Notification {
-                        Id = new Guid("fc1beb5a-5ad0-4deb-b35a-595d81d575e0"),
-                        OccuredAt = new DateTime(2018, 10, 1, 14, 0, 0),
-                        Message = "Notification 1",
-                        Type = notificationType,
-                    }
-                };
+                var receiver = CreateDefaultReceiver();
+                var notifications = CreateDefaultNotifications(notificationType);
 
                 // Act
                 var result = formatter.Format(receiver, notifications);
@@ -61,6 +56,33 @@ namespace Tauchbolde.Tests.DomainServices.Notifications
                 // Assert
                 Approvals.VerifyHtml(result);
             }
+        }
+
+        private static Diver CreateDefaultReceiver()
+        {
+            var receiver = new Diver
+            {
+                Id = new Guid("4c3b714e-522f-4ef8-85f4-db74f0ccdd76"),
+                Fullname = "John Doe",
+                Firstname = "John",
+                Lastname = "Doe",
+            };
+            return receiver;
+        }
+
+        private static List<Notification> CreateDefaultNotifications(NotificationType notificationType)
+        {
+            var notifications = new List<Notification>()
+            {
+                new Notification
+                {
+                    Id = new Guid("fc1beb5a-5ad0-4deb-b35a-595d81d575e0"),
+                    OccuredAt = new DateTime(2018, 10, 1, 14, 0, 0),
+                    Message = "Notification 1",
+                    Type = notificationType,
+                }
+            };
+            return notifications;
         }
     }
 }
