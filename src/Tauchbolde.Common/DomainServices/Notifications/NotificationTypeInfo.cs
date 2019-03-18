2c16798137704dab7dfc146fc172360cbc2365e4
Marc Dürst <marc@marcduerst.com>
2019-03-18 19:42:51 +0100

    F Further improve HTML emails

--- src/Tauchbolde.Common/DomainServices/Notifications/NotificationTypeInfo.cs
+++ src/Tauchbolde.Common/DomainServices/Notifications/NotificationTypeInfo.cs
@@ -3,6 +3,5 @@ namespace Tauchbolde.Common.DomainServices.Notifications
     public class NotificationTypeInfo
     {
         public string Caption { get; set; }
-        public string Color { get; set; }
     }
 }
