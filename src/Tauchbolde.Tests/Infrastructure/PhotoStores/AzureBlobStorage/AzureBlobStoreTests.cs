ed81e30df4b4902259006c4c588179dfdb43af77
Marc Dürst <marc@marcduerst.com>
2019-07-04 20:56:59 +0200

    B Fix get photo from AzureBlob Storage

--- src/Tauchbolde.Tests/Infrastructure/PhotoStores/AzureBlobStorage/AzureBlobStoreTests.cs
+++ src/Tauchbolde.Tests/Infrastructure/PhotoStores/AzureBlobStorage/AzureBlobStoreTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using FakeItEasy;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Tauchbolde.Commom.Misc;
@@ -27,16 +28,35 @@ namespace Tauchbolde.Tests.Infrastructure.PhotoStores.AzureBlobStorage
         public async Task Test_AddPhoto()
         {
             var store = CreateStore();
-            var identifier = new PhotoIdentifier(PhotoCategory.LogbookTeaser, false, "test.jpg");
-            var contentType = "image/jpg";
-            Stream content = new MemoryStream(new byte[] {42, 21, 1});
-            var photo = new Photo(identifier, contentType, content);
+            var photo = CreateSamplePhoto();
 
             await store.AddPhotoAsync(photo);
 
-            A.CallTo(() => logger.Log<object>(LogLevel.Error, A<EventId>._, A<object>._, A<Exception>._,
+            A.CallTo(() => logger.Log(LogLevel.Error, A<EventId>._, A<object>._, A<Exception>._,
                     A<Func<object, Exception, string>>._))
                 .MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task Test_GetPhoto()
+        {
+            var store = CreateStore();
+            var photo = CreateSamplePhoto();
+            await store.AddPhotoAsync(photo);
+
+            var result = await store.GetPhotoAsync(photo.Identifier);
+
+            result.ContentType.Should().Be("image/jpeg");
+            result.Content.Length.Should().Be(photo.Content.Length);
+        }
+
+        private static Photo CreateSamplePhoto()
+        {
+            var identifier = new PhotoIdentifier(PhotoCategory.LogbookTeaser, false, "test.jpg");
+            var contentType = "image/jpeg";
+            Stream content = new MemoryStream(new byte[] {42, 21, 1});
+            
+            return new Photo(identifier, contentType, content);
         }
 
         private AzureBlobStore CreateStore(AzureBlobStoreConfiguration configuration = null)
