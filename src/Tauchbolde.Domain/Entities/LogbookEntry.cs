8a42405963ee627f61bf19db4eaddfae355bf5f8
Marc Dürst <marc@marcduerst.com>
2019-07-21 17:13:10 +0200

    B Guard publish and unpublish LogbookEntries

--- src/Tauchbolde.Domain/Entities/LogbookEntry.cs
+++ src/Tauchbolde.Domain/Entities/LogbookEntry.cs
@@ -12,60 +12,65 @@ namespace Tauchbolde.Domain.Entities
     {
         [DisplayName("Titel")]
         [Required]
-        [NotNull] public string Title { get; set; } = "";
+        [NotNull]
+        public string Title { get; set; } = "";
 
         [DisplayName("Text/Beschreibung")]
         [Required]
-        [NotNull] public string Text { get; set; } = "";
+        [NotNull]
+        public string Text { get; set; } = "";
 
         [DisplayName("Optionaler Teaser/Intro")]
         [Required]
-        [NotNull] public string TeaserText { get; set; } = "";
+        [NotNull]
+        public string TeaserText { get; set; } = "";
 
-        [DisplayName("Favorisierter Eintrag")]
-        public bool IsFavorite { get; set; } = false;
+        [DisplayName("Favorisierter Eintrag")] public bool IsFavorite { get; set; } = false;
 
         [CanBeNull] public string TeaserImage { get; set; }
 
         [CanBeNull] public string TeaserImageThumb { get; set; }
 
         [DisplayName("Optionale Url externer Fotoalbum")]
-        [CanBeNull] public string ExternalPhotoAlbumUrl { get; set; }
+        [CanBeNull]
+        public string ExternalPhotoAlbumUrl { get; set; }
 
         [DisplayName("Erstellt am")]
         [Required]
         public DateTime CreatedAt { get; set; }
-        
-        [DisplayName("Geändert am")]
-        public DateTime? ModifiedAt { get; set; }
 
-        [DisplayName("Publiziert")]
-        public bool IsPublished { get; set; }
+        [DisplayName("Geändert am")] public DateTime? ModifiedAt { get; set; }
+
+        [DisplayName("Publiziert")] public bool IsPublished { get; set; }
 
         public Guid? EditorAuthorId { get; set; }
-        
+
         public Diver EditorAuthor { get; set; }
 
-        [Required]
-        [UsedImplicitly]
-        public Guid OriginalAuthorId { get; set; }
-        
+        [Required] [UsedImplicitly] public Guid OriginalAuthorId { get; set; }
+
         public Diver OriginalAuthor { get; set; }
 
         public Guid? EventId { get; set; }
-        
+
         [CanBeNull] public Event Event { get; set; }
 
         public void Publish()
         {
-            IsPublished = true;
-            RaiseDomainEvent(new LogbookEntryPublishedEvent(Id));
+            if (!IsPublished)
+            {
+                IsPublished = true;
+                RaiseDomainEvent(new LogbookEntryPublishedEvent(Id));
+            }
         }
 
         public void Unpublish()
         {
-            IsPublished = false;
-            RaiseDomainEvent(new LogbookEntryUnpublishedEvent(Id));
+            if (IsPublished)
+            {
+                IsPublished = false;
+                RaiseDomainEvent(new LogbookEntryUnpublishedEvent(Id));
+            }
         }
     }
 }
