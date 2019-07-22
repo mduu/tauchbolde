f52ee42af87db31121ef271851b1220092e46235
Marc Dürst <marc@marcduerst.com>
2019-07-22 20:43:11 +0200

    F Integrate NewLogbookEntry use-case into controller

--- src/Tauchbolde.Web/Controllers/LogbookController.cs
+++ src/Tauchbolde.Web/Controllers/LogbookController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Tauchbolde.Application.OldDomainServices.Logbook;
 using Tauchbolde.Application.OldDomainServices.Users;
+using Tauchbolde.Application.UseCases.Logbook.NewUseCase;
 using Tauchbolde.Application.UseCases.Logbook.PublishUseCase;
 using Tauchbolde.Application.UseCases.Logbook.UnpublishUseCase;
 using Tauchbolde.Domain.Helpers;
@@ -119,21 +120,40 @@ namespace Tauchbolde.Web.Controllers
                 return BadRequest();
             }
 
-            var id = await logbookService.UpsertAsync(new LogbookUpsertModel
+            if (!model.Id.HasValue)
             {
-                Id = model.Id,
-                Text = model.Text,
-                Title = model.Title,
-                TeaserImage = teaserImageStream,
-                TeaserImageFileName = teaserImageFilename,
-                TeaserImageContentType = teaserImageContentType,
-                Teaser = model.Teaser,
-                CreatedAt = model.CreatedAt,
-                IsFavorite = model.IsFavorite,
-                CurrentDiverId = currentDiver.Id,
-                ExternalPhotoAlbumUrl = model.ExternalPhotoAlbumUrl,
-            });
-            await context.SaveChangesAsync();
+                await mediator.Send(new NewLogbookEntry(
+                    currentDiver.Id,
+                    model.Title,
+                    model.Text,
+                    model.Teaser,
+                    model.IsFavorite,
+                    teaserImageStream,
+                    teaserImageFilename,
+                    teaserImageContentType,
+                    model.ExternalPhotoAlbumUrl,
+                    null
+                ));
+            }
+            else
+            {
+                // TODO Replace this block with an explicit edit use-case
+                await logbookService.UpsertAsync(new LogbookUpsertModel
+                {
+                    Id = model.Id,
+                    Text = model.Text,
+                    Title = model.Title,
+                    TeaserImage = teaserImageStream,
+                    TeaserImageFileName = teaserImageFilename,
+                    TeaserImageContentType = teaserImageContentType,
+                    Teaser = model.Teaser,
+                    CreatedAt = model.CreatedAt,
+                    IsFavorite = model.IsFavorite,
+                    CurrentDiverId = currentDiver.Id,
+                    ExternalPhotoAlbumUrl = model.ExternalPhotoAlbumUrl,
+                });
+                await context.SaveChangesAsync();
+            }
 
             ShowSuccessMessage($"Logbucheintrag '{model.Title}' erfolgreich gespeichert.");
 
