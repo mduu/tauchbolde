de1c563879eada24aeef7ae4fa230058125ae4c8
Marc Dürst <marc@marcduerst.com>
2019-07-30 17:25:10 +0200

    !!! Fix project and package references one more

--- src/Tauchbolde.Web/Core/MvcUrlGenerator.cs
+++ src/Tauchbolde.Web/Core/MvcUrlGenerator.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Tauchbolde.Application.Services;
 using Tauchbolde.InterfaceAdapters;
 
 namespace Tauchbolde.Web.Core
