cf49f118be4b1f724a3aa14fd77ac2be9f48d157
Marc Dürst <marc@marcduerst.com>
2019-08-23 07:02:12 +0200

    r Remove unused old service methods

--- src/Tauchbolde.Application/OldDomainServices/Notifications/INotificationService.cs
+++ src/Tauchbolde.Application/OldDomainServices/Notifications/INotificationService.cs
@@ -12,6 +12,5 @@ namespace Tauchbolde.Application.OldDomainServices.Notifications
         Task NotifyForNewEventAsync(Event newEvent, Diver currentUser);
         Task NotifyForChangedEventAsync(Event changedEvent, Diver currentUser);
         Task NotifyForChangedParticipationAsync(Participant participant, Diver participatingDiver, Guid eventId);
-        Task NotifyForEventCommentAsync(Comment comment, Event evt, Diver author);
     }
 }
