33da4d6664cb3f6736a4d7f6022ceff2b078bb33
Marc Dürst <marc@marcduerst.com>
2019-08-25 09:13:49 +0200

    B Fix Participant count ppl handling

--- src/Tauchbolde.Domain/Entities/Participant.cs
+++ src/Tauchbolde.Domain/Entities/Participant.cs
@@ -6,9 +6,11 @@ using Tauchbolde.SharedKernel;
 
 namespace Tauchbolde.Domain.Entities
 {
+    // TODO Make the prop setters "internal"
     public class Participant : EntityBase
     {
-        internal Participant()
+        // TODO Make this constructor "internal"
+        public Participant()
         {
         }
 
@@ -25,7 +27,7 @@ namespace Tauchbolde.Domain.Entities
             ParticipatingDiverId = diverId;
             Status = status;
             BuddyTeamName = buddyTeamName;
-            CountPeople = Math.Min(1, numberOfPeople);
+            CountPeople = Math.Max(1, numberOfPeople);
             Note = note;
             
             RaiseDomainEvent(new ParticipationChangedEvent(Id, EventId, ParticipatingDiverId, Status));
@@ -33,37 +35,37 @@ namespace Tauchbolde.Domain.Entities
         
         [Display(Name = "Anlass ID")]
         [Required]
-        public Guid EventId { get; internal set; }
+        public Guid EventId { get; set; }
         
         [Display(Name = "Anlass")]
         [Required]
-        public Event Event { get; internal set; }
+        public Event Event { get; set; }
 
-        public Guid ParticipatingDiverId { get; internal set; }
+        public Guid ParticipatingDiverId { get; set; }
 
         [Display(Name = "Teilnehmer")]
         [Required]
-        public Diver ParticipatingDiver { get; internal set; }
+        public Diver ParticipatingDiver { get; set; }
 
         [Display(Name = "Anzahl Personen")]
         [Required]
-        public int CountPeople { get; internal set; }
+        public int CountPeople { get; set; }
 
         [Display(Name ="Notiz")]
-        public string Note { get; internal set; }
+        public string Note { get; set; }
 
         [Display(Name ="Status")]
         [Required]
-        public ParticipantStatus Status { get; internal set; }
+        public ParticipantStatus Status { get; set; }
 
         [Display(Name="Buddy Team")]
-        public string BuddyTeamName { get; internal set; }
+        public string BuddyTeamName { get; set; }
 
         public void Edit(ParticipantStatus status, string buddyTeamName, int numberOfPeople, string note)
         {
             Status = status;
             BuddyTeamName = buddyTeamName;
-            CountPeople = Math.Min(1, numberOfPeople);
+            CountPeople = Math.Max(1, numberOfPeople);
             Note = note;
             
             RaiseDomainEvent(new ParticipationChangedEvent(Id, EventId, ParticipatingDiverId, Status));
---
+++ src/Tauchbolde.Tests/Domain/Entities/ParticipantTests.cs
@@ -0,0 +1,106 @@
+using System;
+using FluentAssertions;
+using Tauchbolde.Domain.Entities;
+using Tauchbolde.Domain.Events.Event;
+using Tauchbolde.Domain.Types;
+using Xunit;
+
+namespace Tauchbolde.Tests.Domain.Entities
+{
+    public class ParticipantTests
+    {
+        private readonly Guid validParticipantId = new Guid("40575568-FC15-4966-81A9-046ECA856674");
+        private readonly Guid validEventId = new Guid("6DCAF731-EF4D-4C92-B7AF-FBDE48325B29");
+        private readonly Guid validDiverId = new Guid("43F1E95C-5211-4031-A0FA-EFD95ADE425B");
+
+        [Theory]
+        [InlineData(ParticipantStatus.Accepted)]
+        [InlineData(ParticipantStatus.Declined)]
+        [InlineData(ParticipantStatus.Tentative)]
+        [InlineData(ParticipantStatus.None)]
+        public void Ctor_NewParticipationSuccess(ParticipantStatus status)
+        {
+            // Act
+            var participant = new Participant(validEventId, validDiverId, status, "team1", 1, null);
+
+            // Assert
+            participant.EventId.Should().Be(validEventId);
+            participant.ParticipatingDiverId.Should().Be(validDiverId);
+            participant.Status.Should().Be(status);
+            participant.CountPeople.Should().Be(1);
+            participant.BuddyTeamName.Should().Be("team1");
+            participant.Note.Should().BeNull();
+            participant.UncommittedDomainEvents.Should().ContainSingle(c => c.GetType() == typeof(ParticipationChangedEvent));
+        }
+        
+        [Fact]
+        public void Ctor_NewParticipation_WithNote_Success()
+        {
+            // Act
+            var participant = new Participant(validEventId, validDiverId, ParticipantStatus.Accepted, "team1", 1, "test");
+
+            // Assert
+            participant.EventId.Should().Be(validEventId);
+            participant.ParticipatingDiverId.Should().Be(validDiverId);
+            participant.Status.Should().Be(ParticipantStatus.Accepted);
+            participant.CountPeople.Should().Be(1);
+            participant.BuddyTeamName.Should().Be("team1");
+            participant.Note.Should().Be("test");
+        }
+        
+        [Fact]
+        public void Ctor_NewParticipation_WithtLessThenOnePpl_Success()
+        {
+            // Act
+            var participant = new Participant(validEventId, validDiverId, ParticipantStatus.Accepted, "team1", 0, null);
+
+            // Assert
+            participant.CountPeople.Should().Be(1);
+        }
+        
+        [Fact]
+        public void Ctor_NewParticipation_WithMoreThenOnePpl_Success()
+        {
+            // Act
+            var participant = new Participant(validEventId, validDiverId, ParticipantStatus.Accepted, "team1", 2, null);
+
+            // Assert
+            participant.CountPeople.Should().Be(2);
+        }
+
+        [Theory]
+        [InlineData(ParticipantStatus.Accepted, "team2", 0, null, 1)]
+        [InlineData(ParticipantStatus.Declined, null, 1, "I'm not here", 1)]
+        [InlineData(ParticipantStatus.Tentative, "team2", 2, "I'm propably here", 2)]
+        [InlineData(ParticipantStatus.None, null, -1, null, 1)]
+        public void Edit_Succuss(ParticipantStatus status, string buddyTeam, int countPpl, string note, int expectedCountPpl)
+        {
+            // Arrange
+            var participant = new Participant
+            {
+                Id = validParticipantId,
+                EventId = validEventId,
+                ParticipatingDiverId = validDiverId,
+                BuddyTeamName = "team1",
+                CountPeople = 1,
+                Status = ParticipantStatus.None,
+                Note = null,
+            };
+            
+            // Act
+            participant.Edit(status, buddyTeam, countPpl, note);
+
+            // Assert
+            participant.Status.Should().Be(status);
+            participant.BuddyTeamName.Should().Be(buddyTeam);
+            participant.CountPeople.Should().Be(expectedCountPpl);
+            participant.Note.Should().Be(note);
+            
+            participant.Id.Should().Be(validParticipantId);
+            participant.EventId.Should().Be(validEventId);
+            participant.ParticipatingDiverId.Should().Be(validDiverId);
+
+            participant.UncommittedDomainEvents.Should().ContainSingle(e => e.GetType() == typeof(ParticipationChangedEvent));
+        }
+    }
+}
