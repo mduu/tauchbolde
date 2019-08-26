a38592807f0c7d0347c4ca116522fe6a8796d186
Marc Dürst <marc@marcduerst.com>
2019-08-26 21:17:00 +0200

    F Add LogTelemetryParticipationChangedPolicy

--- src/Tauchbolde.Application/Policies/Event/ParticipationChanged/LogTelemetryParticipationChangedPolicy.cs
+++ src/Tauchbolde.Application/Policies/Event/ParticipationChanged/LogTelemetryParticipationChangedPolicy.cs
@@ -21,6 +21,8 @@ namespace Tauchbolde.Application.Policies.Event.ParticipationChanged
 #pragma warning disable 1998
         public async Task Handle([NotNull] ParticipationChangedEvent notification, CancellationToken cancellationToken)
         {
+            if (notification == null) throw new ArgumentNullException(nameof(notification));
+            
             telemetryService.TrackEvent(TelemetryEventNames.ParticipationChanged, notification);
         }
 #pragma warning restore 1998
