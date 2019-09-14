212761dced6fd7f198ce02eeeb402c8f2d4a2c83
Marc Dürst <marc@marcduerst.com>
2019-09-14 18:07:52 +0200

    F Add PublishNewEventPolicy

---
+++ src/Tauchbolde.Application/Policies/Event/EventCreated/PublishNewEventPolicy.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Tauchbolde.Application.DataGateways;
+using Tauchbolde.Application.Services.Notifications;
+using Tauchbolde.Domain.Events.Event;
+using Tauchbolde.Domain.Types;
+using Tauchbolde.SharedKernel.Extensions;
+
+namespace Tauchbolde.Application.Policies.Event.EventCreated
+{
+    [UsedImplicitly]
+    internal class PublishNewEventPolicy : INotificationHandler<EventCreatedEvent>
+    {
+        [NotNull] private readonly ILogger<PublishNewEventPolicy> logger;
+        [NotNull] private readonly INotificationPublisher notificationPublisher;
+        [NotNull] private readonly IRecipientsBuilder recipientsBuilder;
+        [NotNull] private readonly IEventRepository eventRepository;
+
+        public PublishNewEventPolicy(
+            [NotNull] ILogger<PublishNewEventPolicy> logger,
+            [NotNull] INotificationPublisher notificationPublisher,
+            [NotNull] IRecipientsBuilder recipientsBuilder,
+            [NotNull] IEventRepository eventRepository)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.notificationPublisher = notificationPublisher ?? throw new ArgumentNullException(nameof(notificationPublisher));
+            this.recipientsBuilder = recipientsBuilder ?? throw new ArgumentNullException(nameof(recipientsBuilder));
+            this.eventRepository = eventRepository ?? throw new ArgumentNullException(nameof(eventRepository));
+        }
+
+        public async Task Handle([NotNull] EventCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            if (notification == null) throw new ArgumentNullException(nameof(notification));
+
+            var evt = await eventRepository.FindByIdAsync(notification.EventId);
+            if (evt == null)
+            {
+                logger.LogError("Event [{id}] not found!", notification.EventId);
+                return;
+            }
+
+            var recipients = await recipientsBuilder.GetAllTauchboldeButDeclinedParticipantsAsync(
+                evt.OrganisatorId,
+                evt.Id);
+
+            var message = $"Neue Aktivität '{evt.Name}' ({evt.StartTime.FormatTimeRange(evt.EndTime)}) von {evt.Organisator.Realname} erstellt.";
+
+            await notificationPublisher.PublishAsync(
+                NotificationType.NewEvent,
+                message,
+                recipients,
+                relatedEventId: evt.Id);
+        }
+    }
+}
---
+++ src/Tauchbolde.Tests/Application/Policies/Events/PublishNewEventPolicyTests.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+using Tauchbolde.Application.DataGateways;
+using Tauchbolde.Application.Policies.Event.EventCreated;
+using Tauchbolde.Application.Services.Notifications;
+using Tauchbolde.Domain.Entities;
+using Tauchbolde.Domain.Events.Event;
+using Tauchbolde.Domain.Types;
+using Xunit;
+
+namespace Tauchbolde.Tests.Application.Policies.Events
+{
+    public class PublishNewEventPolicyTests
+    {
+        [NotNull] private readonly PublishNewEventPolicy policy;
+        [NotNull] private readonly ILogger<PublishNewEventPolicy> logger = A.Fake<ILogger<PublishNewEventPolicy>>();
+        [NotNull] private readonly INotificationPublisher notificationPublisher = A.Fake<INotificationPublisher>();
+        [NotNull] private readonly IRecipientsBuilder recipientsBuilder = A.Fake<IRecipientsBuilder>();
+        [NotNull] private readonly IEventRepository eventRepository = A.Fake<IEventRepository>();
+        private readonly Guid validEventId = new Guid("A870DCC0-0F50-48B9-8FCC-1A62365D51CF");
+        private readonly Guid currentUserId = new Guid("97359838-63F0-4A9E-94F1-2B01DB49349F");
+
+        public PublishNewEventPolicyTests()
+        {
+            A.CallTo(() => eventRepository.FindByIdAsync(A<Guid>._))
+                .ReturnsLazily(call => Task.FromResult(
+                    (Guid) call.Arguments[0] == validEventId
+                        ? new Event
+                        {
+                            Id = validEventId,
+                            Name = "Test Event",
+                            Location = "loc",
+                            MeetingPoint = "meet",
+                            Description = "descr",
+                            StartTime = new DateTime(2019, 9, 1, 19, 0, 0),
+                            OrganisatorId = currentUserId,
+                            Organisator = new Diver
+                            {
+                                Id = new Guid("8DCF530A-E55D-4FED-97D7-D636CDAD8BFC"),
+                                Fullname = "John Doe",
+                            }
+                        }
+                        : null));
+
+            A.CallTo(() => recipientsBuilder.GetAllTauchboldeButDeclinedParticipantsAsync(currentUserId, validEventId))
+                .ReturnsLazily(() => Task.FromResult(
+                    new List<Diver>
+                    {
+                        new Diver { Id = new Guid("A73286C3-BD30-4D2B-BAF4-43F7A0EB7B21")},
+                        new Diver { Id = new Guid("6D717184-7A4B-4091-9133-AC539F55C5C4")}
+                    }));
+
+            policy = new PublishNewEventPolicy(logger, notificationPublisher, recipientsBuilder, eventRepository);
+        }
+
+        [Fact]
+        public async Task Handle_Success()
+        {
+            // Arrange
+            EventCreatedEvent notification = CreateNotification();
+           
+            // Act
+            await policy.Handle(notification, CancellationToken.None);
+
+            // Assert
+            A.CallTo(() => notificationPublisher.PublishAsync(
+                    NotificationType.NewEvent,
+                    A<string>._,
+                    A<IEnumerable<Diver>>._,
+                    A<Diver>._,
+                    validEventId,
+                    null))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task Handle_InvalidEventId_MustFail()
+        {
+            // Arrange
+            var notification = CreateNotification(eventId: new Guid("56C45285-782B-4C23-9E10-4877240232E0"));
+           
+            // Act
+            await policy.Handle(notification, CancellationToken.None);
+
+            // Assert
+            A.CallTo(() => notificationPublisher.PublishAsync(
+                    NotificationType.EditEvent,
+                    A<string>._,
+                    A<IEnumerable<Diver>>._,
+                    A<Diver>._,
+                    A<Guid>._,
+                    A<Guid?>._))
+                .MustNotHaveHappened();
+        }
+
+        [Fact]
+        public void Handle_NullNotification_MustThrow()
+        {
+            // Act
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Func<Task> act = () => policy.Handle(null, CancellationToken.None);
+            
+            // Assert
+            act.Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("notification");
+        }
+
+        private EventCreatedEvent CreateNotification(Guid? eventId = null) => new EventCreatedEvent(eventId ?? validEventId);
+    }
+}
