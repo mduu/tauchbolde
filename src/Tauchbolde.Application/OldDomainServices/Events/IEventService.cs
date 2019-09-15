d595bbb86c61524c16ca9bdee5547c7606b9097b
Marc Dürst <marc@marcduerst.com>
2019-09-15 17:33:53 +0200

    r Remove unused code

--- src/Tauchbolde.Application/OldDomainServices/Events/IEventService.cs
+++ src/Tauchbolde.Application/OldDomainServices/Events/IEventService.cs
@@ -7,14 +7,6 @@ namespace Tauchbolde.Application.OldDomainServices.Events
     public interface IEventService
     {
         /// <summary>
-        /// Insert or update the given <paramref name="eventToUpsert"/>.
-        /// </summary>
-        /// <param name="eventToUpsert">Eventdata to insert or update.</param>
-        /// <param name="currentUser">The current users diver record.</param>
-        /// <returns>The inserted or updated <see cref="Event"/>.</returns>
-        Task<Event> UpsertEventAsync(Event eventToUpsert, Diver currentUser);
-
-        /// <summary>
         /// Get the event with all details by its ID.
         /// </summary>
         /// <param name="eventId">The <see cref="Event.Id"/> of the event </param>
