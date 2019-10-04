2715b1b952da51771c84430b2bbeecf27d29abcd
Marc Dürst <marc@marcduerst.com>
2019-10-04 16:16:19 +0200

    !!! Add a first API controller

---
+++ src/Tauchbolde.Web/api/Controllers/EventsApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tauchbolde.Web.Core;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tauchbolde.Web.Api.Controllers
+{
+    [Authorize(Policy = PolicyNames.RequireTauchboldeOrAdmin)]
+    [Route("api/events")]
+    public class EventsApiController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
