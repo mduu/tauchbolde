c93dfd5d0c70234a6e76df7f8afde09f40b2a2c6
Marc Dürst <marc@marcduerst.com>
2019-10-08 04:08:26 +0200

    !!! Deactivate sending auth messages because of spam sender

--- src/Tauchbolde.Web/Services/AuthMessageSender.cs
+++ src/Tauchbolde.Web/Services/AuthMessageSender.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
 using Tauchbolde.Driver.SmtpEmail;
 
 namespace Tauchbolde.Web.Services
@@ -11,17 +12,24 @@ namespace Tauchbolde.Web.Services
     public class AuthMessageSender : IEmailSender
     {
         private readonly IAppEmailSender emailSender;
+        private readonly ILogger logger;
 
         public AuthMessageSender(
-            IAppEmailSender emailSender)
+            IAppEmailSender emailSender,
+            ILogger<AuthMessageSender> logger)
         {
             this.emailSender = emailSender ?? throw new ArgumentNullException(nameof(emailSender));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             var s = $"Tauchbolde Website: {subject}";
-            await emailSender.SendAsync(email, email, s, message);
+            
+            logger.LogWarning("Email would be send: Address={email};Subject={s};Message={message}");
+            
+            // TODO Prefent sending spam
+            //await emailSender.SendAsync(email, email, s, message);
         }
 
         public Task SendSmsAsync(string number, string message)
