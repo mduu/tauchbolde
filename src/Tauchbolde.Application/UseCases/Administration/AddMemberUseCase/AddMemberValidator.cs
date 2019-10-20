52bb3e56521b3dc817b93fa94371ba6b2b733376
Marc Dürst <marc@marcduerst.com>
2019-10-20 19:59:49 +0200

    r Change AddMemberValidator visibillity to 'internal'

--- src/Tauchbolde.Application/UseCases/Administration/AddMemberUseCase/AddMemberValidator.cs
+++ src/Tauchbolde.Application/UseCases/Administration/AddMemberUseCase/AddMemberValidator.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 namespace Tauchbolde.Application.UseCases.Administration.AddMemberUseCase
 {
     [UsedImplicitly]
-    public class AddMemberValidator : AbstractValidator<AddMember>
+    internal class AddMemberValidator : AbstractValidator<AddMember>
     {
         public AddMemberValidator()
         {
